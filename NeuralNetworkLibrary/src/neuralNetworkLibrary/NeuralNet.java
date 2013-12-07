21ff8c47f4b723f60045919fa83949cc2d988a6c
Ali Essam <ali.essam.arafa@gmail.com>
2013-12-07 20:12:27 +0200

    Initial Commit

---
+++ NeuralNetworkLibrary/src/neuralNetworkLibrary/NeuralNet.java
@@ -0,0 +1,7 @@
+package neuralNetworkLibrary;
+
+public class NeuralNet {
+	public NeuralNet() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ OCRANN_Android/gen/com/aaa/ocrann/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.aaa.ocrann;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
