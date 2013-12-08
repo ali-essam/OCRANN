674d8fac8be4176cfb2e6256c9b27cc7ee771ca6
Ali Essam <ali.essam.arafa@gmail.com>
2013-12-08 20:58:32 +0200

    Implemented initialize

--- NeuralNetworkLibrary/src/neuralNetworkLibrary/NeuralNet.java
+++ NeuralNetworkLibrary/src/neuralNetworkLibrary/NeuralNet.java
@@ -5,13 +5,14 @@ import java.util.ArrayList;
 
 public class NeuralNet {
 	private List<Layer> layers;
-
+	private Layer inputLayer;
+	private Layer outputLayer;
 	public NeuralNet() {
 		layers = new ArrayList<Layer>();
 	}
 
 	public List<Double> run(List<Double> input) {
-		// TODO Implement run
+		
 		return null;
 	}
 
@@ -20,8 +21,13 @@ public class NeuralNet {
 			layers.add(layer);
 	}
 
-	public void initialize() {
+	public void initialize() throws Exception {
+		if(layers.size()<2)
+			throw new Exception("There must be at least 2 layers (input & output)");
 		for (int i = 1; i < layers.size(); i++)
 			layers.get(i).initialize(layers.get(i - 1).getOutputVector());
+		
+		inputLayer = layers.get(0);
+		outputLayer = layers.get(layers.size()-1);
 	}
 }
